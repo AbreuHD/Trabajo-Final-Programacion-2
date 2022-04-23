057629afe62023970c894c07fb228c326459092b
Randy D. Baldayaque <baldayaque123@gmail.com>
2022-04-23 12:45:50 -0400

    Validaciones GrupoEntidades y TipoEntidades

--- CapaPresentacion/Pantallas Secundarias/GruposEntidades/PSGrupoEntidades.Designer.cs
+++ CapaPresentacion/Pantallas Secundarias/GruposEntidades/PSGrupoEntidades.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.btnAceptar = new System.Windows.Forms.Button();
             this.lbInfoPantalla = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
@@ -37,14 +38,17 @@
             this.cBEstatus = new System.Windows.Forms.ComboBox();
             this.label3 = new System.Windows.Forms.Label();
             this.cBEliminable = new System.Windows.Forms.CheckBox();
+            this.epError = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.epError)).BeginInit();
             this.SuspendLayout();
             // 
             // btnAceptar
             // 
             this.btnAceptar.DialogResult = System.Windows.Forms.DialogResult.OK;
-            this.btnAceptar.Location = new System.Drawing.Point(193, 391);
+            this.btnAceptar.Location = new System.Drawing.Point(257, 481);
+            this.btnAceptar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnAceptar.Name = "btnAceptar";
-            this.btnAceptar.Size = new System.Drawing.Size(92, 31);
+            this.btnAceptar.Size = new System.Drawing.Size(123, 38);
             this.btnAceptar.TabIndex = 0;
             this.btnAceptar.Text = "button1";
             this.btnAceptar.UseVisualStyleBackColor = true;
@@ -54,9 +58,10 @@
             // 
             this.lbInfoPantalla.AutoSize = true;
             this.lbInfoPantalla.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbInfoPantalla.Location = new System.Drawing.Point(12, 31);
+            this.lbInfoPantalla.Location = new System.Drawing.Point(16, 38);
+            this.lbInfoPantalla.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lbInfoPantalla.Name = "lbInfoPantalla";
-            this.lbInfoPantalla.Size = new System.Drawing.Size(203, 24);
+            this.lbInfoPantalla.Size = new System.Drawing.Size(260, 29);
             this.lbInfoPantalla.TabIndex = 1;
             this.lbInfoPantalla.Text = "Crear Grupo Entidades";
             // 
@@ -64,33 +69,39 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(64, 113);
+            this.label2.Location = new System.Drawing.Point(85, 139);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(87, 18);
+            this.label2.Size = new System.Drawing.Size(110, 24);
             this.label2.TabIndex = 2;
             this.label2.Text = "Descripcion";
             // 
             // tBDescripcion
             // 
-            this.tBDescripcion.Location = new System.Drawing.Point(58, 134);
+            this.tBDescripcion.Location = new System.Drawing.Point(77, 165);
+            this.tBDescripcion.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tBDescripcion.Name = "tBDescripcion";
-            this.tBDescripcion.Size = new System.Drawing.Size(192, 20);
+            this.tBDescripcion.Size = new System.Drawing.Size(255, 22);
             this.tBDescripcion.TabIndex = 3;
+            this.tBDescripcion.Validated += new System.EventHandler(this.tBDescripcion_Validated);
             // 
             // tBComentario
             // 
-            this.tBComentario.Location = new System.Drawing.Point(58, 192);
+            this.tBComentario.Location = new System.Drawing.Point(77, 236);
+            this.tBComentario.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tBComentario.Name = "tBComentario";
-            this.tBComentario.Size = new System.Drawing.Size(192, 20);
+            this.tBComentario.Size = new System.Drawing.Size(255, 22);
             this.tBComentario.TabIndex = 5;
+            this.tBComentario.Validated += new System.EventHandler(this.tBComentario_Validated);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(64, 171);
+            this.label1.Location = new System.Drawing.Point(85, 210);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(86, 18);
+            this.label1.Size = new System.Drawing.Size(107, 24);
             this.label1.TabIndex = 4;
             this.label1.Text = "Comentario";
             // 
@@ -101,36 +112,43 @@
             this.cBEstatus.Items.AddRange(new object[] {
             "Activa",
             "Inactiva"});
-            this.cBEstatus.Location = new System.Drawing.Point(58, 243);
+            this.cBEstatus.Location = new System.Drawing.Point(77, 299);
+            this.cBEstatus.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.cBEstatus.Name = "cBEstatus";
-            this.cBEstatus.Size = new System.Drawing.Size(192, 21);
+            this.cBEstatus.Size = new System.Drawing.Size(255, 24);
             this.cBEstatus.TabIndex = 6;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(65, 222);
+            this.label3.Location = new System.Drawing.Point(87, 273);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(58, 18);
+            this.label3.Size = new System.Drawing.Size(70, 24);
             this.label3.TabIndex = 7;
             this.label3.Text = "Estatus";
             // 
             // cBEliminable
             // 
             this.cBEliminable.AutoSize = true;
-            this.cBEliminable.Location = new System.Drawing.Point(170, 283);
+            this.cBEliminable.Location = new System.Drawing.Point(227, 348);
+            this.cBEliminable.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.cBEliminable.Name = "cBEliminable";
-            this.cBEliminable.Size = new System.Drawing.Size(73, 17);
+            this.cBEliminable.Size = new System.Drawing.Size(92, 20);
             this.cBEliminable.TabIndex = 8;
             this.cBEliminable.Text = "Eliminable";
             this.cBEliminable.UseVisualStyleBackColor = true;
             // 
-            // PSTiposEntidades
+            // epError
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.epError.ContainerControl = this;
+            // 
+            // PSGrupoEntidades
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(310, 450);
+            this.ClientSize = new System.Drawing.Size(413, 554);
             this.Controls.Add(this.cBEliminable);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.cBEstatus);
@@ -140,9 +158,11 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.lbInfoPantalla);
             this.Controls.Add(this.btnAceptar);
-            this.Name = "PSTiposEntidades";
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "PSGrupoEntidades";
             this.Text = "PSGrupoEntidades";
             this.Load += new System.EventHandler(this.PSGrupoEntidades_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.epError)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -159,5 +179,6 @@
         private System.Windows.Forms.ComboBox cBEstatus;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.CheckBox cBEliminable;
+        private System.Windows.Forms.ErrorProvider epError;
     }
 }
